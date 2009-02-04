5c421daaf2bd809170f89f996e7f33bbc78e1d4a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-02-05 00:23:14 +0900

    added docs for driver::memcached

---
+++ script/memcached.pl
@@ -0,0 +1,40 @@
+use strict;
+use warnings;
+use Test::More tests => 2;
+use Neko::DB::User;
+use Data::Model::Driver::Memcached;
+use Cache::Memcached::Fast;
+
+my $dm = Neko::DB::User->new();
+
+# ドライバ情報をつっこみまくる
+{
+    my $driver = Data::Model::Driver::Memcached->new(
+        memcached => Cache::Memcached::Fast->new({
+            servers => [
+                '127.0.0.1:11211',
+            ],
+        }),
+    );
+    for my $model ($dm->schema_names) {
+        $dm->set_driver($model, $driver);
+    }
+}
+
+# INSERT
+$dm->set( 'user' => 1, {
+    name => 'yappo'
+});
+$dm->set('user' => 2, {
+    name => 'ukonmanaho'
+});
+
+# SELECT
+{
+    my ($yappo) = $dm->get('user' => 1);
+    is $yappo->name, 'yappo';
+}
+{
+    my ($ukonmanaho) = $dm->get('user' => 2);
+    is $ukonmanaho->name, 'ukonmanaho';
+}
